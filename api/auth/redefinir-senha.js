19576778593e797c1d21fcce7993460babc3005c
Jovena3000 <146227500+Jovena3000@users.noreply.github.com>
2026-03-23 01:46:55 -0300

    Update redefinir-senha.js

--- api/auth/redefinir-senha.js
+++ api/auth/redefinir-senha.js
@@ -3,7 +3,6 @@ module.exports = async function handler(req, res) {
     const allowedOrigins = [
         'https://karaokemultiplayer.com.br',
         'https://www.karaokemultiplayer.com.br',
-        'https://karaoke-multiplayer.pages.dev',
         'http://localhost:3000'
     ];
     
