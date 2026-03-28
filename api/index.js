8a9c016eb8754eb1ba0192e65ad5e1b744226837
Jovena3000 <146227500+Jovena3000@users.noreply.github.com>
2026-03-28 14:09:32 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
   const allowedOrigins = [
     'https://karaokemultiplayer.com.br',
     'https://www.karaokemultiplayer.com.br',
+    'https://karaoke-multiplayer.pages.dev',
     'http://localhost:3000',
     'http://localhost:8080',
     'http://127.0.0.1:8080'
