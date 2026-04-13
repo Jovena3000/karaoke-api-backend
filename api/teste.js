ed178a7340c06d4db67addc58e7e4e2ccb2cef3e
Jovena3000 <146227500+Jovena3000@users.noreply.github.com>
2026-04-13 15:03:09 -0300

    Create teste.js

---
+++ api/teste.js
@@ -0,0 +1,5 @@
+// api/teste.js
+module.exports = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.status(200).json({ status: 'ok', message: 'API funcionando' });
+};
